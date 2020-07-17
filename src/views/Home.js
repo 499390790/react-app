1af5e5ed60ae93066c51bff07f9baa09cb797e2c
lijinghui <lijinghui@kanzhun.com>
2020-07-17 17:54:31 +0800

    增加基础配置

---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react'
+import { Button } from 'antd'
+class Home extends Component{
+  constructor (props){
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return(
+      <Fragment>
+        <Button type="primary">button</Button>
+      </Fragment>
+    )
+  }
+}
+
+export default Home
